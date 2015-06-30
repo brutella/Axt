875fff353418128059784ca95064cd0b312244df
Harry Jordan <harry@inquisitivesoftware.com>
2015-06-30 12:46:31 +0100

    Updated OCMock to v3.1.2
    Fixed compiler warnings

--- Podfile
+++ Podfile
@@ -3,5 +3,5 @@ platform :ios, '7.0'
 requires_arc = true
 
 target :AxtTests do
-  pod 'OCMock', '~> 2.2'
+	pod 'OCMock', '~> 3.1.2'
 end
--- Pods/Pods-AxtTests-OCMock-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AxtTests_OCMock : NSObject
-@end
-@implementation PodsDummy_Pods_AxtTests_OCMock
-@end
